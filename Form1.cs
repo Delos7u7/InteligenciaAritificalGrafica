d6530dc92f67c0a9f288ae3e5da706a53c25aa10
Jose Ramon <esedelos@hotmail.com>
2024-12-03 11:51:46 -0600

    Clase MLP acabada

--- Form1.cs
+++ Form1.cs
@@ -67,93 +67,51 @@ namespace InteligenciaArtificalGrafica
         public Form1()
         {
             InitializeComponent();
-            nc = new Numcs();
 
-            //MLP.FIT()
-
+            // Configuración de datos
             Vector F1 = new Vector(0, 0);
             Vector F2 = new Vector(0, 1);
             Vector F3 = new Vector(1, 0);
             Vector F4 = new Vector(1, 1);
 
-            Matrix inputs = new Matrix(F1,F2,F3,F4);
-            Matrix outputs = new Matrix(new Vector(0,1,1,0)).T;
+            Matrix inputs = new Matrix(F1, F2, F3, F4);
+            Matrix outputs = new Matrix(new Vector(0, 1, 1, 0)).T;
 
-
+            // Configuración del modelo
             int nInp = 2;
             int nHid = 10;
             int nOut = 1;
             double α = 0.3;
             int epoch = 2500;
-            Matrix W0 = nc.Random(nInp,nHid);
-            Matrix b0 = nc.Random(nHid, 1);
-            Matrix W1 = nc.Random(nHid,nOut);
-            Matrix b1 = nc.Random(nOut, 1);
 
-            for (int i = 0; i < epoch; i++)
+            MLP mlp = new MLP(nInp, nHid, nOut, α, epoch);
+
+            // Entrenamiento
+            mlp.Fit(inputs, outputs);
+
+            // Predicciones
+            TestPredictions(mlp);
+        }
+
+        private void TestPredictions(MLP mlp)
+        {
+            List<Vector> testInputs = new List<Vector>
+    {
+        new Vector(0, 0),
+        new Vector(0, 1),
+        new Vector(1, 0),
+        new Vector(1, 1)
+    };
+
+            List<string> expectedOutputs = new List<string> { "0", "1", "1", "0" };
+
+            for (int i = 0; i < testInputs.Count; i++)
             {
-                for (global::System.Int32 j = 0; j < inputs.nRows; j++)
-                {
-                    
-                    Matrix X = new Matrix(inputs.GetRow(j));
-                    Matrix Y = new Matrix(outputs.GetRow(j));
-                    Matrix Z0 = X.dot(W0) + b0.T;
-                    Matrix A0 = Sigm(Z0);
-                    Matrix Z1 = A0.dot(W1) + b1.T;
-                    Matrix A1 = Sigm(Z1);
-                    Matrix δ1 = (Y - A1) * A1 * (1.0 - A1);
-                    Matrix ΔW1 = α * (δ1.T.dot(A0));
-                    Matrix Δb1 = α * δ1;
-                    Matrix δ0 = δ1.dot(W1.T) * A0 * (-1) * (A0 - 1);
-                    Matrix ΔW0 = α * (δ0.T.dot(X));
-                    Matrix Δb0 = α * δ0;
-                    W0 = W0 + ΔW0.T;
-                    b0 = b0 + Δb0.T;
-                    W1 = W1 + ΔW1.T;
-                    b1 = b1 + Δb1.T;
-
-
-                }
+                Trace.WriteLine($"Predicción para {testInputs[i]} debería dar {expectedOutputs[i]}");
+                Matrix prediction = mlp.Predict(testInputs[i]);
+                Trace.WriteLine("Predicción de la red");
+                prediction.Show();
             }
-
-            //codigo para predict
-            Trace.WriteLine("Predicción para 0,0 debería de dar 0");
-            Matrix X1 = new Matrix(new Vector (0,0));
-            Matrix Z01 = X1.dot(W0) + b0.T;
-            Matrix A01 = Sigm(Z01);
-            Matrix Z11 = A01.dot(W1) + b1.T;
-            Matrix A11 = Sigm(Z11);
-            Trace.WriteLine("Predicción de la red");
-            A11.Show();
-
-            Trace.WriteLine("Predicción para 0,1 debería de dar 1");
-             X1 = new Matrix(new Vector(0, 1));
-             Z01 = X1.dot(W0) + b0.T;
-             A01 = Sigm(Z01);
-             Z11 = A01.dot(W1) + b1.T;
-             A11 = Sigm(Z11);
-            Trace.WriteLine("Predicción de la red");
-            A11.Show();
-
-            Trace.WriteLine("Predicción para 1,0 debería de dar 1");
-            X1 = new Matrix(new Vector(1, 0));
-            Z01 = X1.dot(W0) + b0.T;
-            A01 = Sigm(Z01);
-            Z11 = A01.dot(W1) + b1.T;
-            A11 = Sigm(Z11);
-            Trace.WriteLine("Predicción de la red");
-            A11.Show();
-
-            Trace.WriteLine("Predicción para 1,1 debería de dar 0");
-            X1 = new Matrix(new Vector(1, 1));
-            Z01 = X1.dot(W0) + b0.T;
-            A01 = Sigm(Z01);
-            Z11 = A01.dot(W1) + b1.T;
-            A11 = Sigm(Z11);
-            Trace.WriteLine("Predicción de la red");
-            A11.Show();
-
-
         }
 
         private Matrix Sigm(Matrix matrix)
---
+++ delos/Data/MLP.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using delos.Mathematics;
+using InteligenciaArtificalGrafica.delos.Mathematics;
+
+namespace delos.Data
+{
+    public class MLP
+    {
+        private Matrix W0, W1, b0, b1;
+        private double learningRate;
+        private int epochs;
+
+        public MLP(int nInp, int nHid, int nOut, double α, int epoch)
+        {
+            learningRate = α;
+            epochs = epoch;
+
+            Numcs nc = new Numcs();
+            W0 = nc.Random(nInp, nHid);
+            b0 = nc.Random(nHid, 1);
+            W1 = nc.Random(nHid, nOut);
+            b1 = nc.Random(nOut, 1);
+        }
+
+        public void Fit(Matrix inputs, Matrix outputs)
+        {
+            for (int i = 0; i < epochs; i++)
+            {
+                for (int j = 0; j < inputs.nRows; j++)
+                {
+                    Matrix X = new Matrix(inputs.GetRow(j));
+                    Matrix Y = new Matrix(outputs.GetRow(j));
+                    Matrix Z0 = X.dot(W0) + b0.T;
+                    Matrix A0 = Sigm(Z0);
+                    Matrix Z1 = A0.dot(W1) + b1.T;
+                    Matrix A1 = Sigm(Z1);
+
+                    Matrix δ1 = (Y - A1) * A1 * (1.0 - A1);
+                    Matrix ΔW1 = learningRate * (δ1.T.dot(A0));
+                    Matrix Δb1 = learningRate * δ1;
+                    Matrix δ0 = δ1.dot(W1.T) * A0 * (-1) * (A0 - 1);
+                    Matrix ΔW0 = learningRate * (δ0.T.dot(X));
+                    Matrix Δb0 = learningRate * δ0;
+
+                    W0 = W0 + ΔW0.T;
+                    b0 = b0 + Δb0.T;
+                    W1 = W1 + ΔW1.T;
+                    b1 = b1 + Δb1.T;
+                }
+            }
+        }
+
+        public Matrix Predict(Vector input)
+        {
+            Matrix X = new Matrix(input);
+            Matrix Z0 = X.dot(W0) + b0.T;
+            Matrix A0 = Sigm(Z0);
+            Matrix Z1 = A0.dot(W1) + b1.T;
+            return Sigm(Z1);
+        }
+
+        private Matrix Sigm(Matrix matrix)
+        {
+            Vector data = matrix.GetRow(0);
+            Vector sigm = 1 / (1 + ((Math.E) ^ (data * -1.0)));
+            Matrix m = new Matrix(sigm);
+            return m;
+        }
+    }
+}
